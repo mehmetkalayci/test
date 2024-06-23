4fe35f9abfb2c64d3ae5e17d791a4ab61677d627
Mehmet <mmtkalayci@gmail.com>
2024-06-23 12:49:31 +0300

    mount metodu eklendi

--- my-widget.es.js
+++ my-widget.es.js
@@ -1,17 +1,20 @@
-import { ref as n, openBlock as p, createElementBlock as _, createElementVNode as a, toDisplayString as r, createApp as l } from "vue";
-const i = (t, o) => {
+import { ref as c, openBlock as i, createElementBlock as p, createElementVNode as a, toDisplayString as r, createApp as _ } from "vue";
+const l = (t, o) => {
   const e = t.__vccOpts || t;
-  for (const [s, c] of o)
-    e[s] = c;
+  for (const [n, s] of o)
+    e[n] = s;
   return e;
-}, m = { class: "widget-container" }, u = {
+}, u = { class: "widget-container" }, d = {
   __name: "App",
   setup(t) {
-    const o = n("My Widget Title"), e = n("This is a message from the Vue.js widget.");
-    return (s, c) => (p(), _("div", m, [
+    const o = c("My Widget Title"), e = c("This is a message from the Vue.js widget.");
+    return (n, s) => (i(), p("div", u, [
       a("h1", null, r(o.value), 1),
       a("p", null, r(e.value), 1)
     ]));
   }
-}, d = /* @__PURE__ */ i(u, [["__scopeId", "data-v-bb870ad6"]]);
-l(d).mount("#app");
+}, m = /* @__PURE__ */ l(d, [["__scopeId", "data-v-bb870ad6"]]);
+function g(t) {
+  _(m).mount(t);
+}
+window.mountWidget = g;
